e5ef80673d1aabbe95c351a489d8582aab953920
Khaing Su Myat Noe <30749679+khaingsumyatnoe-nus@users.noreply.github.com>
2019-12-19 23:23:08 +0800

    Add files via upload
    Asp.net mvc project

---
+++ CustomPasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace ClothesLine
+{
+    public class CustomPasswordHasher : IPasswordHasher
+    {
+       public string HashPassword(string password)
+        {
+            return Encryption.GetMD5Hash(password);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword)
+        {
+            if (hashedPassword == HashPassword(providedPassword))
+                return PasswordVerificationResult.Success;
+            else
+                return PasswordVerificationResult.Failed;
+        }
+    }
+
+    
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClothesLine.MvcApplication" Language="C#" %>
